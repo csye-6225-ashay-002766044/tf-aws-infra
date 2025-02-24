8e315f58e718931aa79c22a88819a793c72103cc
Ashay Saoji <144743549+ashaysaoji@users.noreply.github.com>
2025-02-24 17:56:36 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "public_subnets" {
   count = length(var.public_subnet_cidrs)
 
   vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = var.public_subnet_cidrs[count.index]
+  cidr _block              = var.public_subnet_cidrs[count.index]
   availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = true
 
